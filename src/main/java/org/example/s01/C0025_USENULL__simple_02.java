5c5763751047148f0f3c16f3f2d4c7f982b16cb9
taeyoung <taeyoung@codemind.co.kr>
2025-07-24 18:30:01 +0900

    Add 0025 검출 파일 2

---
+++ src/main/java/org/example/s01/C0025_USENULL__simple_02.java
@@ -0,0 +1,41 @@
+package org.example.s01;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class C0025_USENULL__simple_02 {
+
+    public static int bad (Object obj, final Collection collection) {
+        int count = 2;
+        if (collection == null) {
+            return count;
+        }
+        Iterator it = collection.iterator();
+
+        while (it.hasNext()) {
+            Object elt = it.next();
+            /* FLAW: CWE-476 */
+            if ((null == obj && null == elt) || obj.equals(elt)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int good (Object obj, final Collection col) {
+        int count = 0;
+        if (col == null) {
+            return count;
+        }
+        Iterator it = col.iterator();
+
+        while (it.hasNext()) {
+            Object elt = it.next();
+            /* FIX : Null Check */
+            if ((null == obj && null == elt) || (null != obj && obj.equals(elt))) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
